ab22e72ac6ee09cf7427f5d26bbc4408ed476a6a
Muhammad Rizwan <xrizwanr@gmail.com>
2022-11-29 09:24:45 +0500

    cleaned up code

--- api/celery/worker.py
+++ api/celery/worker.py
@@ -1,26 +1,12 @@
 from celery import Celery
 from fastapi import UploadFile
-from time import sleep
 from api.helpers import merge_images, save_images, OrientationType
 from typing import List
 
-# class CeleryConfig:
-#     broker="amqp://guest:guest@localhost:5672/"
-#     backend="rpc://"
-
-#     task_serializer = 'json'
-#     result_serializer = 'json'
-#     accept_content = ['json']
-#     enable_utc = True
 
 celery_app = Celery('collage_api_tasks', backend="rpc://", broker='amqp://guest@localhost//', accept_content = ['json', 'pickle'], result_accept_content = ['json', 'pickle'])
 celery_app.set_default()
 celery_app.autodiscover_tasks(["api"])
-
-@celery_app.task(name="add")
-def add(x, y):
-    sleep(60)
-    return x + y
 
 @celery_app.task(name="generate_image")
 def generate_image(images: List[UploadFile], orientation: OrientationType):
@@ -28,9 +14,5 @@ def generate_image(images: List[UploadFile], orientation: OrientationType):
     generated_image_namge = merge_images(saved_images_path, orientation)
     return generated_image_namge
 
-
-# celery_app.config_from_object(CeleryConfig)
-
-
-# python -m flower --app=tasks.celery_app worker -E
+# run celery
 # celery -A api.celery.worker.celery_app worker --loglevel=info -P eventlet
--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 from api.routers import collage
-# from api.celery.celery_app import celery_app
 
 origins = ['*']
 
@@ -16,7 +15,6 @@ app.add_middleware(
     expose_headers=["*"],
 )
 app.include_router(collage.router)
-# app.celery = celery_app
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
